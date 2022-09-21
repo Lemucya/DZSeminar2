b579eda45d064bd2f0316408a855973beffec9b9
Lemucya <lemucya@mail.ru>
2022-09-21 20:51:10 +0700

    Написали программу по решению задачи10

---
+++ zadacha10/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Напишите программу, которая принимает на вход трёхзначное число 
+и на выходе показывает вторую цифру этого числа.*/
+Console.WriteLine("Введите трехзначное число: ");
+int number=Convert.ToInt32(Console.ReadLine());
+
+int getNumberTwoisTree(int number)
+{
+int result = new int();
+result = number % 100;
+result /= 10;
+return result;
+}
+
+if(number > 99 & number <1000)
+{
+    int number2 = getNumberTwoisTree(number);
+    Console.WriteLine(number2);
+}
